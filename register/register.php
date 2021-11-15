5c0915c5f03d85fc91ab86aeebc7c6a5676f28de
Mateusz Błażków <matebla682@student.polsl.pl>
2021-11-15 22:25:10 +0100

    Widoki nawigacji i rejestracji

---
+++ register/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="registerArea">
+    <h1>
+        MAJTECZKI W KROPECZKI
+    </h1>
+    <form class ="register" action="" method="post">
+        <input type="email" id="email" name="email" placeholder="E-mail"><br>
+        <input type="password" id="password" name="password" placeholder="Hasło"><br>
+        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło"><br>
+    </form>
+    <div class="seperator"></div>
+    </div>
+    
+    
+</body>
+</html>
