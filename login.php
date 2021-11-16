e7a018eea0bc374dca28d2dd14cda9cc6073310c
Julia Plocienniczak <julaplocienniczak@gmail.com>
2021-11-16 22:36:46 +0100

    login init

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zaloguj się</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="loginArea">
+    <h1>
+        MAJTECZKI W KROPECZKI
+    </h1>
+    <h2>Zaloguj się na swoje konto</h2>
+    
+    <form class ="login" action="" method="post">
+        <input type="email" id="email" name="email" placeholder="E-mail"><br>
+        <input type="password" id="password" name="password" placeholder="Hasło"><br>
+        <button type="submit">Zaloguj</button>
+    </form>
+    <div class="seperator"></div>
+    </div>
+    
+    
+</body>
+</html>
