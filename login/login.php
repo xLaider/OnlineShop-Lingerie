956c9704fef3852283b0335879a5799704f90b84
Rafał Miczek <rafal.miczek@t2t-system.com>
2021-11-17 21:37:34 +0100

    Revert "Merge branch 'main' of https://github.com/xLaider/OnlineShop-Lingerie"
    This reverts commit 9f039b4a9d068eb98ef771f6b8e22bb5859e7b84, reversing
    changes made to 103ff056bcac4d973aefe81f093dbdf7258a0f95.

--- login/login.php
+++ login/login.php
@@ -1,15 +1,5 @@
-<?php
-    session_start();
-    if(isset($_SESSION['logged_email']))
-    {
-        header('Location: mainPage');
-        exit();
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,35 +7,31 @@
     <title>Zaloguj się</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
     <div class="loginArea">
-        <h1>
-            MAJTECZKI W KROPECZKI
-            </h1>
+    <h1>
+        MAJTECZKI W KROPECZKI
+    </h1>
     <h2><?php
             if (isset($_GET['registerSuccessful'])) 
             echo 'Rejestracja przebiegła pomyślnie!<br>'
         ?>
         Zaloguj sie na swoje konto!
-        </h2>
-        <?php
-        if (isset($_SESSION['bad_attempt'])) {
-        ?>
-            <p class="error">Wprowadzono błędne dane. Spróbuj ponownie</p>
-        <?php
-            unset($_SESSION['bad_attempt']);
-        }
-        ?>
-        <form class="login" action="login_form_handling.php" method="post">
-            <input type="email" id="email" name="email" placeholder="E-mail"><br>
-            <input type="password" id="password" name="password" placeholder="Hasło"><br>
-            <button type="submit">Zaloguj</button>
-        </form>
-        <div class="seperator"></div>
-  
+    </h2>
+    <form class ="login" action="login_form_handling.php" method="post">
+
+        <input type="email" id="email" name="email" placeholder="E-mail"><br>
+        <input type="password" id="password" name="password" placeholder="Hasło"><br>
+        <input type="submit" value="Zaloguj">
+    </form>
+    <div class="seperator">
+    Jeśli nie masz jeszcze założoneg konta<br>
+    <a href="index.php?redirectToRegister">kliknij tutaj</a>
+    
     </div>
-
+    </div>
+    
+    <?php session_start();unset($_SESSION['bad_attempt']);?>
+    
 </body>
-
 </html>
