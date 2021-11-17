ecceb9ff5cfdee8214f66848f519ee09f409f889
Rafał Miczek <rafal.miczek@t2t-system.com>
2021-11-17 20:46:53 +0100

    Changed navbar.

--- navBar/navBar.php
+++ navBar/navBar.php
@@ -5,20 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <nav>
-    <img class="menu-icon" src="./images/menu.svg" alt="Menu">
-        <div class="logo">
-            <a href="index.php">MAJTECZKI W KROPECZKI</a>
-        </div>
-        <div class="text">
-            <a href="index.php?redirectToLogin">Zaloguj</a>
-            |
-            <img class="" src="./images/cart.svg" alt="Menu">
-        </div>
-        
-    </nav>
+<nav class="header">
+    <img class="menu-icon" src="../assets/images/menu.svg" alt="Menu"  onclick="openNav()">
+                
+                <h1 class="logo">MAJTECZKI W KROPECZKI</h1>
+                <a href="#about">ZALOGUJ</a>
+    <img class="menu-icon" src="../assets/images/cart.svg" alt="Menu"></span>
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Kobieta</a>
+                <a href="#">Mężczyzna</a>
+                <a href="#">Dzieci</a>
+            </div>
+
+        </nav>
+
+        <script>
+        //Sidebar
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+
+
+    </script>
 </body>
 </html>
