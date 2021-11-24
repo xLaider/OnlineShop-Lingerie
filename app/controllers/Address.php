3ae21a244f960d5ec6889952664e1c94f381c3e0
Julia Plocienniczak <julaplocienniczak@gmail.com>
2021-11-24 22:46:25 +0100

    address v3

--- app/controllers/Address.php
+++ app/controllers/Address.php
@@ -9,35 +9,44 @@ class Address extends Controller
     }
     public function index()
     {
+        $address = $this->shipmentModel->getUserAddress($_SESSION['userData']->AddressId);
+        $_SESSION['address'] = $address;
         $this->view('address');
+        
     }
 
     public function addressHandler()
     {
-        print_r($_POST);
         if (!isset($_POST['country']) || !isset($_POST['city']) || !isset($_POST['street']) || !isset($_POST['number']) || !isset($_POST['postCode']) || strlen($_POST['country']) == 0 || strlen($_POST['city']) == 0 || strlen($_POST['street']) == 0 || strlen($_POST['number']) == 0 || strlen($_POST['postCode']) == 0) {
 
             $_SESSION['errorEmptyAddress'] = "Uzupełnij wszystkie pola1!";
             header("Location: " . URLROOT . "/address");
             exit();
+
         } else {
-
-
             unset($_SESSION['errorEmptyAddress']);
-            print_r($_SESSION);
 
             //pobranie z bazy adresu po id address z tabeli users
-            $addressId = $this->shipmentModel->getUserAddress($_SESSION['userData']->AddressId);
-            if ($addressId == null) {//nie ma adresu
+            $address = $this->shipmentModel->getUserAddress($_SESSION['userData']->AddressId);
+           
+            if ($address == null) {//jeśli adresu nie ma to utworzenie go
                 $this->shipmentModel->createAddress($_POST['country'], $_POST['city'], $_POST['street'], $_POST['number'], $_POST['postCode'], $_SESSION['userData']->email);
-                echo "sds";
+
             } else {
                 //jak jest to go updateuje
-                //...UpdateAddress($_POST['country'], $_POST['city'], $_POST['street'], $_POST['number'], $_POST['postCode'], $addressId)
+
+                
+                $this->shipmentModel->updateAddress($_POST['country'], $_POST['city'], $_POST['street'], $_POST['number'], $_POST['postCode'], $_SESSION['userData']->AddressId);
                 unset($_SESSION['errorUser']);
-                echo "dsd";
                 exit();
                 //else czyli jeśli znalazł to ma być wypełnione wierszami z bazy
+           
+               
+                //w ulicy chociaż filtr alfanumeryczny
+                //ikonka profilu na stronie głównej
+                
+                //chyba nie powinno sie dac zalogowac bedac zalogowanym
+                //gdzie ma być zamknięcie sesji , wydaje mi sie ze bylo w wylogowaniu a nie ma nigdzie
                 // header("Location: ".URLROOT."/login");
                 // exit();
             }
