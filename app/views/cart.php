307fc659d97749290afddcde2edcf873aedb6159
Julia Plocienniczak <julaplocienniczak@gmail.com>
2021-11-28 12:59:12 +0100

    duplicating the header in files fix

--- app/views/cart.php
+++ app/views/cart.php
@@ -1,26 +1,8 @@
 <link rel="stylesheet" href="./css/navBar.css" />
 <link rel="stylesheet" href="./css/cart.css" /></head>
-<nav class="header">
-    <img class="menu-icon" src="./assets/images/menu.svg" alt="Menu"  onclick="openNav()">
-                
-    <h1 class="logo">
-        <a href="<?php echo URLROOT?>">MAJTECZKI W KROPECZKI</a>
-    </h1>
-    <?php if (isset($_SESSION['userData'])) :?>
-        <a href="<?php echo URLROOT."/index/logout"?>">WYLOGUJ</a>
-    <?php else:?>
-        <a href="<?php echo URLROOT."/login"?>">ZALOGUJ</a>
-    <?php endif; ?>
-    <a href="<?php echo URLROOT."/cart"?>">
-        <img class="menu-icon" src="./assets/images/cart.svg" alt="Menu"></span>
-    </a>
-    <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">Kobieta</a>
-        <a href="#">Mężczyzna</a>
-        <a href="#">Dzieci</a>
-    </div>
-</nav>
+<?php
+    include "header.php";
+?>
 <script>
         //Sidebar
     function openNav() {
--- app/views/order.php
+++ app/views/order.php
@@ -5,27 +5,9 @@
 
 </head>
 <main>
-    <nav class="header">
-        <img class="menu-icon" src="./assets/images/menu.svg" alt="Menu" onclick="openNav()">
-
-        <h1 class="logo">
-            <a href="<?php echo URLROOT ?>">MAJTECZKI W KROPECZKI</a>
-        </h1>
-        <?php if (isset($_SESSION['userData'])) : ?>
-            <a href="<?php echo URLROOT . "/index/logout" ?>">WYLOGUJ <?php  ?></a>
-        <?php else : ?>
-            <a href="<?php echo URLROOT . "/login" ?>">ZALOGUJ</a>
-        <?php endif; ?>
-        <a href="<?php echo URLROOT . "/cart" ?>">
-            <img class="menu-icon" src="./assets/images/cart.svg" alt="Menu"></span>
-        </a>
-        <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="#">Kobieta</a>
-            <a href="#">Mężczyzna</a>
-            <a href="#">Dzieci</a>
-        </div>
-    </nav>
+<?php
+    include "header.php";
+?>
     <script>
         //Sidebar
         function openNav() {
