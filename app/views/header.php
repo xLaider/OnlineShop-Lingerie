9896cb350b2466d98eb2deb229fa7c6f47fd7c00
Rafał Miczek <miczek.r@gmail.com>
2021-12-04 23:05:49 +0100

    Design changes in navbar.

--- app/views/header.php
+++ app/views/header.php
@@ -1,20 +1,26 @@
 <nav class="header">
-    <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/menu.svg" alt="Menu"  onclick="openNav()">
-                
+    <div class="left">
+        <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/menu.svg" alt="Menu"  onclick="openNav()">
+    </div>
+                <div class="right flex-row">
+                    <?php if (isset($_SESSION['userData'])) :?>
+                        <a href="<?php echo URLROOT."/index/logout"?>">WYLOGUJ</a>
+                    <?php else:?>
+                        <a href="<?php echo URLROOT."/login"?>">ZALOGUJ</a>
+                    <?php endif; ?>
+                    <strong>|</strong>
+                    <a href="<?php echo URLROOT."/cart"?>">
+                        <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/cart.svg" alt="Cart"></span>
+                    </a>
+                    <?php if (isset($_SESSION['userData'])) :?>
+                    <a href="<?php echo URLROOT."/profile"?>">
+                        <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/user-icon.svg" alt="Profil"></span>
+                    </a>
+                    <?php endif; ?>
+                </div>
     <h1 class="logo">
         <a href="<?php echo URLROOT?>">MAJTECZKI W KROPECZKI</a>
     </h1>
-    <?php if (isset($_SESSION['userData'])) :?>
-        <a href="<?php echo URLROOT."/index/logout"?>">WYLOGUJ</a>
-    <?php else:?>
-        <a href="<?php echo URLROOT."/login"?>">ZALOGUJ</a>
-    <?php endif; ?>
-    <a href="<?php echo URLROOT."/cart"?>">
-        <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/cart.svg" alt="Cart"></span>
-    </a>
-    <a href="<?php echo URLROOT."/profile"?>">
-        <img class="menu-icon" src="<?php echo URLROOT;?>/assets/images/user-icon.svg" alt="Profil"></span>
-    </a>
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="#">Kobieta</a>
