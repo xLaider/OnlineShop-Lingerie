9f998628cf2125fefab91c7fbb4bc180967547c6
Rafał Miczek <miczek.r@gmail.com>
2021-12-04 22:55:32 +0100

    Design changes in authorization.

--- app/views/login.php
+++ app/views/login.php
@@ -1,27 +1,26 @@
-<link rel="stylesheet" href="<?php echo URLROOT;?>/css/login.css">
+<link rel="stylesheet" href="<?php echo URLROOT;?>/css/authorization.css">
 </head>
 
 <div class="loginArea">
     <h1>
-        MAJTECZKI W KROPECZKI
+        <a href="index.php">MAJTECZKI W KROPECZKI</a>
     </h1>
     <h2><?php
             if (isset($_GET['registerSuccessful'])) 
             echo 'Rejestracja przebiegła pomyślnie!<br>'
         ?>
-        Zaloguj sie na swoje konto!
     </h2>
     <form class ="login" action="<?php echo URLROOT."/login/tryToLogin"?>" method="post">
 
-        <input type="email" id="email" name="email" placeholder="E-mail"><br>
-        <input type="password" id="password" name="password" placeholder="Hasło"><br>
+        <input type="email" <?php if(isset($_SESSION['errorEmptyLogin'])||isset($_SESSION['errorBadAuthorize']))echo "class='error'";?> id="email" name="email" placeholder="E-mail"><br>
+        <input type="password" <?php if(isset($_SESSION['errorEmptyLogin'])||isset($_SESSION['errorBadAuthorize']))echo "class='error'";?> id="password" name="password" placeholder="Hasło"><br>
         <input type="submit" value="Zaloguj">
     </form>
     <?php
     if (isset($_SESSION['errorEmptyLogin']))echo $_SESSION['errorEmptyLogin'];
     if (isset($_SESSION['errorBadAuthorize']))echo $_SESSION['errorBadAuthorize'];
     ?>
-    <div class="seperator">
+    <div class="seperator"></div>
     Jeśli nie masz jeszcze założonego konta<br>
     <a href="<?php echo URLROOT."/register"?>">kliknij tutaj</a>
     
--- app/views/register.php
+++ app/views/register.php
@@ -1,14 +1,14 @@
-<link rel="stylesheet" href="<?php echo URLROOT;?>/css/register.css">
+<link rel="stylesheet" href="<?php echo URLROOT;?>/css/authorization.css">
 </head>
 <body>
     <div class="registerArea">
     <h1>
-        MAJTECZKI W KROPECZKI
+        <a href="index.php">MAJTECZKI W KROPECZKI</a>
     </h1>
     <form class ="register" action="<?php echo URLROOT."/register/registerUser"?>" method="post">
-        <input type="email" id="email" name="email" placeholder="E-mail"><br>
-        <input type="password" id="password" name="password" placeholder="Hasło"><br>
-        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło"><br>
+        <input type="email" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="email" name="email" placeholder="E-mail"><br>
+        <input type="password" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="password" name="password" placeholder="Hasło"><br>
+        <input type="password" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło"><br>
         <input type="submit" value="Zarejestruj">
     </form>
     <?php
