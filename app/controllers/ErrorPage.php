b56679ea032b626f4dd560f9222d035338c9d6ee
Your Name <you@example.com>
2021-12-05 20:39:24 +0100

    add error page :(

---
+++ app/controllers/ErrorPage.php
@@ -0,0 +1,8 @@
+<?php
+
+class ErrorPage extends Controller {
+    
+    public function index(){
+        $this->view('errorPage');
+    }
+}
---
+++ app/views/errorPage.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/errorPage.css" />
+
+<div class=errorPage>
+    <section>
+        <h1>UPS...</h1>
+        <h4>Coś poszło nie tak :(</h4>
+    </section>
+    <section>
+        <img src="<?php echo URLROOT; ?>/assets/images/sadcat.svg">
+    </section>
+
+</div>
